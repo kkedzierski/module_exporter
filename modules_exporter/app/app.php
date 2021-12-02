101b8384676cd6f4f2c6086798c13ec038b4ba9e
kkedzierski <kamil.kedzierski@onet.com.pl>
2021-12-02 06:19:21 +0100

    create initializeArtisanCommand method

--- modules_exporter/app/app.php
+++ modules_exporter/app/app.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types = 1);
-
+namespace App\Console\Commands;
 include_once("../src/classes/ModulesExport.php");
 
 $modulesExport = new ModulesExport();
+$modulesExport->initializeArtisanCommand();
 
--- modules_exporter/src/classes/ModulesExport.php
+++ modules_exporter/src/classes/ModulesExport.php
@@ -1,9 +1,34 @@
 <?php
 
+namespace App\Console\Commands;
 class ModulesExport{
 
     public function __construct(){
         var_dump("test");
     }
 
+    /**
+     * Create custom artisan command, after call this you are able to call: php artisan export-modules
+     *
+     * @param string  $directoryPath path to artisan commands directory
+     * @throws echo   if path are not choosen correctly
+     * 
+     * @return void
+     */ 
+    public function initializeArtisanCommand(string $directoryPath='../../app/Console/Commands/'): void {
+        if(!is_dir($directoryPath)){
+            mkdir($directoryPath, 0755, true);
+        }
+
+        $exportModulesCommandPath = '../../modules_exporter/src/classes/ExportModulesCommand.php';
+        $exportModulesCommandName = 'ExportModulesCommand.php';
+        try {
+            copy($exportModulesCommandPath, $directoryPath . $exportModulesCommandName);
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+        
+    }
+
 }
+
